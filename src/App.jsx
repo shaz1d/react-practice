870281d8c974b9dd25bf7a399a1f84a280989096
shaz1d <shazidulislam8443@gmail.com>
2022-04-17 21:59:21 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="nav">
+      <Navbar></Navbar>
+      <ul>
+        <li>Hello</li>
+        <li>World</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
